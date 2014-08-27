f971d6d5da776a4de634c4e7c0b3a838c743d424
geovanips <geovanips@outlook.com>
2014-08-27 16:27:52 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, i;
+    cout << " Digite uma quantidade para que possamos contar.:  ";
+    cin >> n;
+    i=0;
+    while (i<=n)
+    {
+    cout << " Out: " << i <<"\n";
+    i++;
+    }
+}
+
